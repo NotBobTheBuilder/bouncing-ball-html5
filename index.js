7039b55d7cd1a2b0363844105d7c3aa52584d098
Jack Wearden <jack@jackwearden.co.uk>
2014-02-05 15:01:34 +0000

    Woah how cool is this thing innit wow such glory

---
+++ index.js
@@ -0,0 +1,32 @@
+var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d"),
+    blob = {
+        "x":  10,
+        "y":  10,
+        "xd": 10,
+        "yd": 10,
+        "w": 20,
+        "h": 20,
+    };
+
+function tick() {
+  blob.x = blob.x + blob.xd;
+  blob.y = blob.y + blob.yd;
+
+  ctx.clearRect(0,0, canvas.width, canvas.height)  
+  ctx.fillRect(blob.x, blob.y, blob.w, blob.h);
+
+  if (blob.x <= 0 || blob.x + blob.w > canvas.width)
+    blob.xd = -1 * blob.xd;
+  if (blob.y <= 0 || blob.y + blob.h > canvas.height)
+    blob.yd = -1 * blob.yd;
+
+  window.requestAnimationFrame(tick);
+}
+
+window.requestAnimationFrame(tick);
+
+window.onload = window.onresize = function() {
+  canvas.width  = window.innerWidth - 20;
+  canvas.height = window.innerHeight -20;
+};
